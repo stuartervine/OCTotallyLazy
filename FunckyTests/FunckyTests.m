1449decbfe5945b9de77d3bbdc3608057a95f456
Stuart Ervine <spervine@gmail.com>
2012-01-30 08:25:36 +0000

    Initial Commit

---
+++ FunckyTests/FunckyTests.m
@@ -0,0 +1,32 @@
+//
+//  FunckyTests.m
+//  FunckyTests
+//
+//  Created by Stuart Ervine on 30/01/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "FunckyTests.h"
+
+@implementation FunckyTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in FunckyTests");
+}
+
+@end
