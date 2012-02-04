586ba412bac625c76281d5f80adafb62757f4d68
Stuart Ervine <spervine@gmail.com>
2012-02-04 16:21:23 +0000

    stu: Adding convenience functions for creating arrays, got fed up with square brackets.

--- Funcky/NSArray+Funcky.h
+++ Funcky/NSArray+Funcky.h
@@ -1,6 +1,22 @@
 #import <Foundation/Foundation.h>
 #import "Option.h"
 #import "Mappable.h"
+#import "Enumerations.h"
+
+static NSArray * array() {
+    return [NSArray array];
+}
+
+static id arrayWith(id items, ...) {
+    NSMutableArray *array = [NSMutableArray array];
+    va_list args;
+    va_start(args, items);
+    for (id arg = items; arg != nil; arg = va_arg(args, id)) {
+        [array addObject:arg];
+    }
+    va_end(args);
+    return array;
+}
 
 @interface NSArray (Functional) <Mappable>
 - (NSArray *)filter:(BOOL (^)(id))filterBlock;
--- FunckyTests/NSArrayTest.m
+++ FunckyTests/NSArrayTest.m
@@ -14,75 +14,72 @@
 @implementation NSArrayTest
 
 - (void)testFilter {
-    NSArray *const items = [NSArray arrayWithObjects:@"a", @"ab", @"b", @"bc", nil];
-    assertThat([items filter:^(NSString *item) {
-        return [item hasPrefix:@"a"];
-    }], equalTo([NSArray arrayWithObjects:@"a", @"ab", nil]));
+    NSArray *const items = arrayWith(@"a", @"ab", @"b", @"bc", nil);
+    assertThat([items filter:^(NSString *item) { return [item hasPrefix:@"a"]; }], equalTo(arrayWith(@"a", @"ab", nil)));
 }
 
 - (void)testFlatMap {
-    NSArray *items = [NSArray arrayWithObjects:
-            [NSArray arrayWithObjects:@"one", @"two", nil],
-            [NSArray arrayWithObjects:@"three", @"four", nil],
-            nil];
-    assertThat([items flatMap:[Callables toUpperCase]], equalTo([NSArray arrayWithObjects:@"ONE", @"TWO", @"THREE", @"FOUR", nil]));
+    NSArray *items = arrayWith(
+            arrayWith(@"one", @"two", nil),
+            arrayWith(@"three", @"four", nil),
+            nil);
+    assertThat([items flatMap:[Callables toUpperCase]], equalTo(arrayWith(@"ONE", @"TWO", @"THREE", @"FOUR", nil)));
 }
 
 - (void)testFold {
-    NSArray *items = [NSArray arrayWithObjects:@"one", @"two", @"three", nil];
+    NSArray *items = arrayWith(@"one", @"two", @"three", nil);
     assertThat([items fold:@"" with:[Callables appendString]], equalTo(@"onetwothree"));
 }
 
 - (void)testHead {
-    NSArray *items = [NSArray arrayWithObjects:@"one", @"two", @"three", nil];
+    NSArray *items = arrayWith(@"one", @"two", @"three", nil);
     assertThat([items head], equalTo(@"one"));
 }
 
 - (void)testHeadOption {
-    NSArray *items = [NSArray arrayWithObjects:@"one", @"two", @"three", nil];
+    NSArray *items = arrayWith(@"one", @"two", @"three", nil);
     assertThat([items headOption], equalTo([Some some:@"one"]));
-    assertThat([[NSArray array] headOption], equalTo([None none]));
+    assertThat([array() headOption], equalTo([None none]));
 }
 
 - (void)testJoin {
-    NSArray *join = [[NSArray arrayWithObjects:@"one", nil] join:[NSArray arrayWithObjects:@"two", @"three", nil]];
-    assertThat(join, equalTo([NSArray arrayWithObjects:@"one", @"two", @"three", nil]) );
-
+    NSArray *join = [arrayWith(@"one", nil) join:arrayWith(@"two", @"three", nil)];
+    assertThat(join, equalTo(arrayWith(@"one", @"two", @"three", nil)));
 }
 
 - (void)testMap {
-    NSArray *items = [NSArray arrayWithObjects:@"one", @"two", @"three", nil];
-    assertThat([items map:[Callables toUpperCase]], equalTo([NSArray arrayWithObjects:@"ONE", @"TWO", @"THREE", nil]));
+    NSArray *items = arrayWith(@"one", @"two", @"three", nil);
+    assertThat([items map:[Callables toUpperCase]], equalTo(arrayWith(@"ONE", @"TWO", @"THREE", nil)));
 }
 
 - (void)testReduce {
-    NSArray *items = [NSArray arrayWithObjects:@"one", @"two", @"three", nil];
+    NSArray *items = arrayWith(@"one", @"two", @"three", nil);
     assertThat([items reduce:[Callables appendString]], equalTo(@"onetwothree"));
 }
 
 - (void)testTail {
-    NSArray *items = [NSArray arrayWithObjects:@"one", @"two", @"three", nil];
-    assertThat([items tail], equalTo([NSArray arrayWithObjects:@"two", @"three", nil]));
+    NSArray *items = arrayWith(@"one", @"two", @"three", nil);
+    assertThat([items tail], equalTo(arrayWith(@"two", @"three", nil)));
 }
 
 - (void)testTake {
-    NSArray *items = [NSArray arrayWithObjects:@"one", @"two", @"three", nil];
-    assertThat([items take:2], equalTo([NSArray arrayWithObjects:@"one", @"two", nil]));
-    assertThat([items take:0], equalTo([NSArray array]));
+    NSArray *items = arrayWith(@"one", @"two", @"three", nil);
+    assertThat([items take:2], equalTo(arrayWith(@"one", @"two", nil)));
+    assertThat([items take:0], equalTo(array()));
 }
 
 - (void)testTakeWhile {
-    NSArray *items = [NSArray arrayWithObjects:[NSNumber numberWithInt:1], [NSNumber numberWithInt:2], [NSNumber numberWithInt:3], nil];
+    NSArray *items = arrayWith([NSNumber numberWithInt:1], [NSNumber numberWithInt:2], [NSNumber numberWithInt:3], nil);
     assertThat(
         [items takeWhile:^(NSNumber *number) { return (BOOL)(number.intValue < 2); }],
-        equalTo([NSArray arrayWithObjects:[NSNumber numberWithInt:1], nil])
+        equalTo(arrayWith([NSNumber numberWithInt:1], nil))
     );
 }
 
 - (void)testTakeRight {
-    NSArray *items = [NSArray arrayWithObjects:@"one", @"two", @"three", nil];
-    assertThat([items takeRight:2], equalTo([NSArray arrayWithObjects:@"two", @"three", nil]));
-    assertThat([items takeRight:0], equalTo([NSArray array]));
+    NSArray *items = arrayWith(@"one", @"two", @"three", nil);
+    assertThat([items takeRight:2], equalTo(arrayWith(@"two", @"three", nil)));
+    assertThat([items takeRight:0], equalTo(array()));
 }
 
 @end
