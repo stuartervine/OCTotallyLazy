d3bd179ae77bbbad0e6293604e00448914bcaa3d
Albert Latacz <voxavion@gmail.com>
2012-07-12 09:01:41 +0100

    Relaxed type restriction in EnumerateEnumerator to allow creating lazy generators (e.g. fibonacci sequence)

--- src/main/enumerators/EnumerateEnumerator.h
+++ src/main/enumerators/EnumerateEnumerator.h
@@ -1,10 +1,8 @@
 #import <Foundation/Foundation.h>
 
 @interface EnumerateEnumerator : NSEnumerator
-@property(nonatomic) NSNumber *seed;
+- (EnumerateEnumerator *)initWithCallable:(id (^)(id))aCallableFunc seed:(id)aSeed;
 
-- (EnumerateEnumerator *)initWithCallable:(id (^)(NSNumber *))aCallableFunc seed:(NSNumber *)aSeed;
-
-+ (EnumerateEnumerator *)withCallable:(id (^)(NSNumber *))callableFunc seed:(NSNumber *)aSeed;
++ (EnumerateEnumerator *)withCallable:(id (^)(id))callableFunc seed:(id)aSeed;
 
 @end
