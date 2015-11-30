712d42af90c989fffa01208974aafcf8486b3e67
stuartervine <stuart.ervine@shift-f6.com>
2015-11-30 10:57:21 +0000

    Adding missing files from new project.

---
+++ OCTotallyLazyTests/OCTotallyLazyTests.m
@@ -0,0 +1,33 @@
+//
+//  OCTotallyLazyTests.m
+//  OCTotallyLazyTests
+//
+//  Created by Stuart Ervine on 13/11/2015.
+//  Copyright © 2015 Stuart Ervine. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OCTotallyLazyTests : XCTestCase
+
+@end
+
+@implementation OCTotallyLazyTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+    XCTAssertTrue(true, "True should be true");
+}
+
+@end
